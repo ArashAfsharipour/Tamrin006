7fd292280c26b2e2bbfef99c0706fbf53deeb2f6
AFSHAR <AFSHAR@DESKTOP-H088APG>
2024-11-02 23:39:41 +0330

    Add project files.

---
+++ Tamrin06/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Yek adad vared konid: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Adad mosbat ast.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Adad manfi ast.");
+        }
+        else
+        {
+            Console.WriteLine("Adad sefr ast.");
+        }
+    }
+}
